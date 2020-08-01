cb9526e1f9c8d6278b982b767f30f11a8c4f484a
steve <stevegazzojr@gmail.com>
2020-07-31 22:32:02 -0400

    Eliminated popup dialog, need new strategy to handle bad data

--- app/src/main/java/com/projectdave/brewtility/services/ABVCalcService.java
+++ app/src/main/java/com/projectdave/brewtility/services/ABVCalcService.java
@@ -23,29 +23,21 @@ public class ABVCalcService {
             finalGravity = Float.valueOf(finalGravityIn);
         }
         catch(NumberFormatException e){
-            generateBadNumberDialog("Please ensure that both boxes contain a valid number");
+            handleBadNumbers("Please ensure that both boxes contain a valid number");
             return 0;
         }
         if(startingGravity <= 0 || finalGravity <= 0){
-            generateBadNumberDialog("Please ensure that starting and final gravity are greater than zero");
+            handleBadNumbers("Please ensure that starting and final gravity are greater than zero");
             return 0;
         }
         if(finalGravity - startingGravity <= 0){
-            generateBadNumberDialog("Starting gravity cannot be greater than final gravity");
+            handleBadNumbers("Starting gravity cannot be greater than final gravity");
             return 0;
         }
         return abvCalculator.calcABVAsPercentage(startingGravity, finalGravity);
     }
 
-    private void generateBadNumberDialog(String message){
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
-        alertDialogBuilder.setMessage(message);
-        alertDialogBuilder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                return;
-            }
-        });
-        alertDialogBuilder.show();
+    private void handleBadNumbers(String message){
+
     }
 }
